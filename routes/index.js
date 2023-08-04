e22674bdeeb8ed44f2278d1e7c718ab5db633ed5
Donato Zollo <davinazarra@gmail.com>
2023-08-04 00:29:30 -0300

    Model & Schema: AudioVideo + Controller

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import audioVideo_router from './audiovideo.js';
 let router = express.Router()
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/audiovideo', audioVideo_router)
 export default router
