abd1f8a30e8c37f2adc8a502a1fe559d2d9a64b9
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-08-03 21:49:39 -0300

    controllers

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import phoneTabs_router from './phoneTabs.js'
 let router = express.Router()
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/phoneTabs' ,phoneTabs_router)
 export default router
