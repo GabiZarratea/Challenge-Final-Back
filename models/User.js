7a46ac19239aee50e2be5c307bd0ed1d1c97df29
Donato Zollo <davinazarra@gmail.com>
2023-08-06 12:54:10 -0300

    añadido de location a el modelo User (para envíos)

--- models/User.js
+++ models/User.js
@@ -2,13 +2,14 @@ import { Schema,model } from 'mongoose'
 
 let collection = 'users'
 let schema = new Schema({
-    email: { type:String,required:true },
-    password: { type:String,required:true },
-    photo: { type:String,required:true },
-    role: { type:Number,required:true },
+    email: { type:String, required:true },
+    password: { type:String, required:true },
+    photo: { type:String, required:true },
+    location: { type: String, required: true },
+    role: { type:Number, required:true, default: 0 },
     online: { type:Boolean },
-    verified: { type:Boolean,required:true },
-    verify_code: { type:String,required:true }
+    verified: { type:Boolean, required:true },
+    verify_code: { type:String, required:true }
 },{
     timestamps:true
 })
