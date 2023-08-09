7bb4a277c544c93148936a8e992445d3e60ddab0
Donato Zollo <davinazarra@gmail.com>
2023-08-09 13:38:16 -0300

    a

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,14 @@ import userRegister from "../schemas/user/register.js";
 import accountExists from "../middlewares/accountExists.js";
 import createHash from "../middlewares/createHash.js";
 import signout from "../controllers/users/signout.js";
+import verifyCode from "../controllers/users/verifyCode.js";
+
 const user_router = Router();
 
 user_router.get('/', read);
 user_router.post("/signout", signout);
 user_router.post("/signin", validator(userSignin), generateToken,signin);
 user_router.post('/register', validator(userRegister), accountExists, createHash,register);
+user_router.get('/verify/:verify_code', verifyCode)
 
 export default user_router;
