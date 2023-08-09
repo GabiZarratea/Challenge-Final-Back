4c62b8aade2f92caaa61f6bd8ef21b4f79a192f5
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2023-08-09 14:05:11 -0300

    Update register.js

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,65 +1,22 @@
-import User from '../../models/User.js';
-import { google } from 'googleapis';
-import nodemailer from 'nodemailer';
-
-const OAuth2 = google.auth.OAuth2;
-
-const oauth2Client = new OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "http://localhost:5173"
-);
-
-oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN,
-});
-
-const accessToken = oauth2Client.getAccessToken();
-
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: "mingamangamh@gmail.com",
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: accessToken,
-    },
-});
-
-// Función para generar un código de verificación aleatorio
-function generateVerificationCode() {
-    const length = 6; // Longitud del código de verificación (puedes ajustarla según tus necesidades)
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let code = '';
-    for (let i = 0; i < length; i++) {
-        code += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return code;
-}
+import User from "../../models/User.js";
 
 export default async (req, res, next) => {
+    const { firebaseUrl } = req.file ? req.file : '';
+    req.body.photo = firebaseUrl
+
     try {
-        // Generar un código de verificación para el usuario
-        const verificationCode = generateVerificationCode();
+        const one = await User.create(
+            { ...req.body });
 
-        // Crear el usuario en la base de datos con el código de verificación
-        const newUser = await User.create({ ...req.body, role: 1, verify_code: verificationCode });
-
-        // Enviar el correo de verificación
-        const mailOptions = {
-            from: 'ccgi@gmail.com',
-            to: newUser.email,
-            subject: 'User Verification',
-            text: `Your verification code is: ${verificationCode}`,
-        };
-
-        const info = await transporter.sendMail(mailOptions);
-        console.log('E-mail send:', info.response);
-
-        return res.status(201).json({ response: newUser, success: true, message: 'User created successfully' });
+        return res.status(201).json({
+            response: { ...one._doc, photo: firebaseUrl},
+            success: true,
+            message: 'User created'
+        })
     } catch (error) {
-        next(error);
-    }
-};
+        return res.status(500).json({
+            response: null,
+            success: false,
+            message: error.message
+    })
+}}
