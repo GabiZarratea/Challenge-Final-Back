4776a36151719f1e068913dc39daa1def8c00551
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2023-08-14 23:28:08 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ let collection = 'users'
 let schema = new Schema({
     email: { type:String, required:true },
     password: { type:String, required:true },
-    photo: { type:String, required: true },
+    photo: { type:String, required: false },
     location: { type: String, required: false },
     role: { type:Number, default: 0 },
     online: { type:Boolean, default: false },
